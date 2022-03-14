fdcc43a4c8b1eebaf9346056b1815161d95f77f2
Alex Romero <alex1812r@gmail.com>
2022-03-13 20:39:34 -0400

    use mongo uri variable

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,11 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { ClientsModule } from './modules/clients/clients.module';
 import { ProductsModule } from './modules/products/products.module';
 import { OrdersModule } from './modules/orders/orders.module';
+import { MONGODB_URI } from './shared/env-variables';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/crm-test'),
+    MongooseModule.forRoot(MONGODB_URI),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: 'schema.gql',
